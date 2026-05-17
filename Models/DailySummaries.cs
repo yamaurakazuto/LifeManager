d9bd9c53a74e6d22352a403246278f373cde8309
Kazuto Yamaura <xyst510@icloud.com>
2026-05-17 14:50:01 +0900

    Update README

--- Models/DailySummaries.cs
+++ Models/DailySummaries.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace LifeManager.Models
 {
     class DailySummaries
     {
+        public int ID { get; set; }
+
+        public DateTime TargetDate { get; set; }
+
+        public decimal Income { get; set; } 
+        public decimal Expense { get; set; }  
+        
+        public decimal Total { get; set; }
+
+        public DateTime UpdateDate { get; set; }
     }
 }
